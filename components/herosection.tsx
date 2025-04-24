36f1832e4764dcb9f8022f723fc1f7d0139ed203
Hnq <hancu345@gmail.com>
2025-04-24 13:10:19 +0300

    Navbar done

---
+++ components/herosection.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Hero() {
+   return <div className='h-[600px] w-full bg-black'></div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import Navbar from "../../components/navbar";
+import HeroSection from "../../components/herosection";
 
 export default function LandingPage() {
    return (
       <div className='min-h-screen flex flex-col'>
          <Navbar />
+         <HeroSection />
       </div>
    );
 }
