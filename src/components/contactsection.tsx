2957174036682dae6d368e67adf45f1b1157638e
Hnq <hancu345@gmail.com>
2025-05-09 08:57:18 +0300

    contact section done (#38)

--- src/components/contactsection.tsx
+++ src/components/contactsection.tsx
@@ -6,12 +6,12 @@ export default function ContactSection() {
    return (
       <main>
          <section id='contact'>
-            <div className='px-15 py-16 text-white bg-green-800 flex flex-col gap-10'>
+            <div className='px-6 xl:px-15 py-16 text-white bg-green-800 flex flex-col gap-10'>
                <div className='flex flex-col gap-5'>
-                  <h1 className='tracking-tight text-4xl font-bold'>
+                  <h1 className='text-center sm:text-left tracking-tight text-4xl font-bold'>
                      Contactați-ne
                   </h1>
-                  <p className='text-xl max-w-[50%]'>
+                  <p className='hidden md:block text-xl max-w-[50%]'>
                      Suntem aici pentru a răspunde întrebărilor dumneavoastră și
                      pentru a vă ajuta să alegeți cei mai potriviți pomi pentru
                      nevoile dumneavoastră.
@@ -19,13 +19,13 @@ export default function ContactSection() {
                </div>
                <div className='flex flex-col gap-3'>
                   <div className='flex flex-row gap-2 items-center'>
-                     <MapPin className='h-6 w-6' />
+                     <MapPin className='h-6 w-6 shrink-0' />
                      <p className='text-xl'>
                         Calea Adjudului 210, Onești, jud. Bacău, România
                      </p>
                   </div>
                   <div className='flex flex-row gap-2 items-center'>
-                     <Phone className='h-6 w-6' />
+                     <Phone className='h-6 w-6 shrink-0' />
                      <p className='text-xl'>+40 743 329 612</p>
                   </div>
                </div>
